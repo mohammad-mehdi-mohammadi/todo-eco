83fb63a9f33b76f11aba0d67b362a6cde65d7a4c
Mohammad Mahdi Mohammadi <phoneticem@gmail.com>
2023-12-22 02:53:58 +0330

    home page - header

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,30 @@
-import {Button} from "@mui/material";
+import {Button, Container, Grid, TextField} from "@mui/material";
 
 const Home = () => {
 
     return (
         <>
-            <Button variant="contained">Home</Button>
+            <Container maxWidth="md">
+                <Grid container>
+                    <Grid xs={12} md = {10}>
+                        <TextField
+                            error
+                            id="outlined-error-helper-text"
+                            label="Text..."
+                            helperText="Incorrect entry."
+                            size="small"
+                            fullWidth
+
+                        />
+                    </Grid>
+                    <Grid xs={12} md = {2}>
+                        <Button fullWidth variant="contained" size="medium">Add</Button>
+                    </Grid>
+
+                </Grid>
+
+            </Container>
+
         </>
     )
 }
